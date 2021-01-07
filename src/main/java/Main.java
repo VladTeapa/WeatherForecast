6d3d3f1aad6b0cc35cd7c0cba0b1341db0832e7f
vladteapa <georgehariga@yahoo.com>
2021-01-08 00:05:52 +0200

    :sparkle: Modified the project
    Issue: #5
    Modified because it wouldn't run!
    
    Signed-off-by: vladteapa <georgehariga@yahoo.com>

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
