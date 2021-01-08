e13af1966ea136f5cc95da6f25fdebe276e03bac
vladteapa <georgehariga@yahoo.com>
2021-01-08 03:15:02 +0200

    :beer: Added label change
    Issue: #5
    
    Added label change and http request
    
    Signed-off-by: vladteapa <georgehariga@yahoo.com>

--- src/main/java/ro/mta/se/lab/controller/WeatherApiController.java
+++ src/main/java/ro/mta/se/lab/controller/WeatherApiController.java
@@ -1,4 +1,37 @@
 package ro.mta.se.lab.controller;
 
+import org.json.JSONObject;
+import ro.mta.se.lab.model.Settings;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class WeatherApiController {
+
+    public WeatherApiController() {
+
+    }
+
+    public JSONObject getWeather(String city) {
+        JSONObject jsonObject = null;
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + Settings.apiKey))
+                    .build();
+            HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            jsonObject = new JSONObject(response.body().toString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(jsonObject.toString());
+        return jsonObject;
+    }
 }
--- src/main/java/ro/mta/se/lab/controller/WeatherViewController.java
+++ src/main/java/ro/mta/se/lab/controller/WeatherViewController.java
@@ -1,9 +1,12 @@
 package ro.mta.se.lab.controller;
 
 import javafx.event.EventType;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import org.json.JSONObject;
 import ro.mta.se.lab.model.Country;
 import ro.mta.se.lab.model.Settings;
 
@@ -11,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class WeatherViewController {
+
     public static void initializeMenuButton(Scene scene, List<Country> countryList){
         MenuButton menuButton = (MenuButton) scene.lookup("#countryDropBox");
         if(menuButton == null){
@@ -26,11 +30,25 @@ public class WeatherViewController {
                 menuButton.setText(menuItem.getText());
                 MenuButton cityMenuButton = (MenuButton) scene.lookup("#cityDropBox");
                 cityMenuButton.getItems().clear();
+                cityMenuButton.setText("City");
                 for(int j = 0;j<country.getCityList().size();j++){
                     MenuItem item = new MenuItem(country.getCityList().get(j).getDenumire());
                     item.setOnAction(e->{
                         System.out.println("Changed city!");
                         cityMenuButton.setText(item.getText());
+                        WeatherApiController weatherApiController = new WeatherApiController();
+                        JSONObject jsonObject = weatherApiController.getWeather(item.getText());
+
+                        Label temperatureLabel = (Label) scene.lookup("#temperatureLabel");
+                        Label humidityLabel = (Label) scene.lookup("#humidityLabel");
+                        Label windSpeedLabel = (Label) scene.lookup("#windSpeedLabel");
+                        Label pressureLabel = (Label) scene.lookup("#pressureLabel");
+
+                        temperatureLabel.setText(String.format("%.1f", (jsonObject.getJSONObject("main").getDouble("temp") - 272.15)) +" \u2103");
+                        humidityLabel.setText(String.format("%.1f", (jsonObject.getJSONObject("main").getDouble("humidity"))) +"%");
+                        windSpeedLabel.setText(String.format("%.1f Km/H", (jsonObject.getJSONObject("wind").getDouble("speed")*3.6)));
+                        pressureLabel.setText(String.format("%.1f hPa", (jsonObject.getJSONObject("main").getDouble("pressure"))));
+
                     });
                     cityMenuButton.getItems().add(item);
                     System.out.println(item.getText());
